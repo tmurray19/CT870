b86618c1c2de1f0024ce986435eff8cba4711d2c
Taidgh Murray <31845904+tmurray19@users.noreply.github.com>
2020-02-04 13:14:11 +0000

    Update utils.js
    Updating vulnerabilities

--- models/utils.js
+++ models/utils.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://mongodb4983mt:ra1pot@danu7.it.nuigalway.ie:8717/mongodb4983', {useNewUrlParser: true, useUnifiedTopology: true});
+// The mongoDB credentials here should be changed, this is just for academic purpose, and the account has likely been deleted by now
+var connection = mongoose.connect('mongodb://mongodb4983mt:ra(3-2)(pot)@danu(5+2).it.nuigalway.ie:(8717)/mongodb4983', {useNewUrlParser: true, useUnifiedTopology: true});
 
 exports.connection = connection;
